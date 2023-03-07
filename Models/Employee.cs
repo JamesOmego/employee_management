777b4f5917cafd22bb89e84379aa1daa69cccb3d
Obadiah <korir400@gmail.com>
2023-03-07 22:58:24 +0300

    This is the setups

---
+++ Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesManagement.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public required string EmployeeNo { get; set; }
+
+        public required string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public required string LastName { get; set; }
+
+        public required  string EmailAddress { get; set; }
+    }
+}
